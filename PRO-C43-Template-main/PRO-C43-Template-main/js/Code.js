edcca749125c5f23990926cd8cd96af0b594c7f2
poisonouspro <82662898+poisonouspro@users.noreply.github.com>
2021-07-24 19:01:12 -0400

    Add files via upload

---
+++ PRO-C43-Template-main/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("CUTNIFON",700,160);
+    text("ATADASBA",100,360);
+    text("Hint: Performs a certain task !!",700,180);
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("Hint: Stores info !!", 100,380);
+    
+
+}
